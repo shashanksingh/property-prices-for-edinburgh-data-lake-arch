485a04aad2b05285341bf264a6d9f5ae562d4e9d
shashank singh <shashank.personal@gmail.com>
2021-06-09 01:46:33 +0100

    added

--- workflow/dags/geography_downloader.py
+++ workflow/dags/geography_downloader.py
@@ -5,8 +5,8 @@ import os
 
 download_geography_data_url = "https://api.os.uk/downloads/v1/products/OpenNames/downloads?area=GB&format=CSV&redirect"
 LOCAL_FILE_NAME = "opname_csv_gb.zip"
-
-
+EXECUTION_DATE = "2021-08-09/"
+DATA_FOLDER = "data/"
 def download_file(url: str, file_name: str):
     with requests.get(url, stream=True) as r:
         with open(file_name, 'wb') as f:
@@ -15,7 +15,7 @@ def download_file(url: str, file_name: str):
 
 def unzip_file(file_name: str):
     with zipfile.ZipFile(file_name, "r") as zip_ref:
-        zip_ref.extractall(LOCAL_FILE_NAME.split(".")[0])
+        zip_ref.extractall(f"{DATA_FOLDER}{EXECUTION_DATE}{LOCAL_FILE_NAME.split('.')[0]}")
 
 
 def remove_zip_file(file_name: str):
