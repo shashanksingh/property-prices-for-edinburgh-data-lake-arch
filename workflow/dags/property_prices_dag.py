14ab9badc8a6e47c090459d7352ca7b25dd8cb83
shashank singh <shashank.personal@gmail.com>
2021-06-10 23:23:03 +0100

    fixes

--- workflow/dags/property_prices_dag.py
+++ workflow/dags/property_prices_dag.py
@@ -6,9 +6,8 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonVirtualenvOperator
 from datetime import datetime, timedelta
 
-import geography_downloader
-import get_rightmove_prices
-import silver_geography_dimension
+# import get_rightmove_prices
+# import silver_geography_dimension
 
 default_args = {
     'owner': 'airflow',
@@ -27,27 +26,33 @@ default_args = {
 
 dag = DAG('property_prices_dag', default_args=default_args, schedule_interval=timedelta(days=1))
 
+
+def geography_downloader_run():
+    print("geography_downloader_run")
+    import geography_downloader
+    geography_downloader.run()
+
+
 geography_downloader = PythonVirtualenvOperator(
     task_id='geography_downloader',
-    python_callable=geography_downloader.run,
-    requirements=["rightmove-webscraper==0.4.0", "requests==2.22.0", "pandas==1.2.4"],
+    python_callable=geography_downloader_run,
+    requirements=["requests==2.22.0", "pandas==1.2.4"],
     system_site_packages=False,
     dag=dag)
+#
+# get_rightmove_prices = PythonVirtualenvOperator(
+#     task_id='get_rightmove_prices',
+#     python_callable=get_rightmove_prices.run,
+#     requirements=["rightmove-webscraper==0.4.0", "requests==2.22.0", "pandas==1.2.4"],
+#     system_site_packages=False,
+#     dag=dag)
 
-get_rightmove_prices = PythonVirtualenvOperator(
-    task_id='get_rightmove_prices',
-    bash_command=get_rightmove_prices.run,
-    requirements=["rightmove-webscraper==0.4.0", "requests==2.22.0", "pandas==1.2.4"],
-    system_site_packages=False,
-    dag=dag)
+# need to put sensors here
+# silver_geography_dimension = PythonVirtualenvOperator(
+#     task_id='silver_geography_dimension',
+#     python_callable=silver_geography_dimension.run,
+#     requirements=["pandas==1.2.4"],
+#     system_site_packages=False,
+#     dag=dag)
 
-#need to put sensors here
-silver_geography_dimension = PythonVirtualenvOperator(
-    task_id='silver_geography_dimension',
-    bash_command=silver_geography_dimension.run,
-    requirements=["rightmove-webscraper==0.4.0", "requests==2.22.0", "pandas==1.2.4"],
-    system_site_packages=False,
-    dag=dag)
-
-
-geography_downloader >> get_rightmove_prices >> silver_geography_dimension
+geography_downloader
