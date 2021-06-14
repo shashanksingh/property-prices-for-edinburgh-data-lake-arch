6ef8bddde28badf483cd5718ca5f8433e70d69d0
shashank singh <shashank.personal@gmail.com>
2021-06-14 17:58:56 +0100

    now atleast first task is downloading

--- workflow/dags/property_prices_dag.py
+++ workflow/dags/property_prices_dag.py
@@ -20,8 +20,8 @@ default_args = {
     # 'priority_weight': 10,
     # 'end_date': datetime(2016, 1, 1),
     'schedule_interval': '@daily',
-    'concurrency': 10,
-    'max_active_runs': 5,
+    'concurrency': 1,
+    'max_active_runs': 1,
 }
 
 dag = DAG('property_prices_dag', default_args=default_args)
@@ -32,20 +32,24 @@ def geography_downloader_run(execution_date, **kwargs):
     import shutil
     import zipfile
     import os
+    from pathlib import Path
 
     download_geography_data_url = "https://api.os.uk/downloads/v1/products/OpenNames/downloads?area=GB&format=CSV&redirect"
-    LOCAL_FILE_NAME = "opname_csv_gb.zip"
-    EXECUTION_DATE = f"{execution_date}/"
-    DATA_FOLDER = "/opt/airflow/data/bronze/"
+    zipped_file_name = "opname_csv_gb.zip"
+    execution_date_path = f"{execution_date}"
+    data_path = "/opt/airflow/data/bronze"
+    full_dir = Path(f'{data_path}/{execution_date_path}/')
+    full_dir.mkdir(parents=True, exist_ok=True)
 
     def download_file(url: str, file_name: str):
         with requests.get(url, stream=True) as r:
             with open(file_name, 'wb') as f:
                 shutil.copyfileobj(r.raw, f)
+        print("download_file")
 
-    def unzip_file(file_name: str):
+    def unzip_file(output_dir: str, file_name: str):
         with zipfile.ZipFile(file_name, "r") as zip_ref:
-            zip_ref.extractall(f"{DATA_FOLDER}{EXECUTION_DATE}{LOCAL_FILE_NAME.split('.')[0]}")
+            zip_ref.extractall(f"{output_dir}{file_name.split('.')[0]}")
 
     def remove_zip_file(file_name: str):
         if os.path.isfile(file_name):
@@ -53,32 +57,31 @@ def geography_downloader_run(execution_date, **kwargs):
         else:
             print(f"file doesnt exist {file_name}")
 
-    try:
-        download_file(download_geography_data_url, LOCAL_FILE_NAME)
-        unzip_file(LOCAL_FILE_NAME)
-        remove_zip_file(LOCAL_FILE_NAME)
-    except requests.exceptions.RequestException as e:
-        print(e)
+    download_file(download_geography_data_url, zipped_file_name)
+    unzip_file(full_dir, zipped_file_name)
+    remove_zip_file(zipped_file_name)
 
 
 def get_rightmove_prices_run(execution_date, **kwargs):
     print(" get_rightmove_prices_run")
-    import rightmove_webscraper
-
+    from rightmove_webscraper import RightmoveData
+    from pathlib import Path
 
     rightmove_url = "https://www.rightmove.co.uk/property-for-sale/find.html?searchType=SALE&locationIdentifier=REGION%5E94346"
-    EXECUTION_DATE = f"{execution_date}/"
-    DATA_FOLDER = "data/bronze/"
-    TABLE_NAME = "property.csv"
-    rm = rightmove_webscraper.rightmove_data(rightmove_url)
+    execution_date_path = f"{execution_date}"
+    data_path = "data/bronze"
+    table_name = "property.csv"
+    full_dir = Path(f'{data_path}/{execution_date_path}/{table_name}')
+    full_dir.mkdir(parents=True, exist_ok=True)
+
+    rm = RightmoveData(rightmove_url)
     df_property_prices = rm.get_results
-    df_property_prices.to_csv(f"{DATA_FOLDER}{EXECUTION_DATE}{TABLE_NAME}")
+    df_property_prices.to_csv(f"{full_dir}")
 
 
 def silver_geography_dimension_run(execution_date, **kwargs):
-    print("silver_geography_dimension")
-    import silver_geography_dimension
-    silver_geography_dimension.run()
+    execution_date_path = f"{execution_date}/"
+    print(f"silver_geography_dimension {execution_date_path}")
 
 
 geography_downloader = PythonVirtualenvOperator(
@@ -109,4 +112,5 @@ silver_geography_dimension = PythonVirtualenvOperator(
     op_kwargs={'execution_date': '{{ execution_date }}', },
     dag=dag)
 
-geography_downloader >> get_rightmove_prices >> silver_geography_dimension
+# check if file exists, if not download
+geography_downloader.set_downstream([get_rightmove_prices, silver_geography_dimension])
