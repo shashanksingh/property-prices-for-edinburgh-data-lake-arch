cd2075ab5edc43119e85d54a94a49506a225d2d0
shashank singh <shashank.personal@gmail.com>
2021-06-22 19:59:31 +0100

    now terraform is able to upload files

--- terraform/servers.tf
+++ terraform/servers.tf
@@ -6,30 +6,42 @@ resource "aws_instance" "airflow" {
   subnet_id = aws_subnet.subnet-uno.id
   security_groups = [aws_security_group.ingress-all-test.id]
 
-//  connection {
-//    type     = "ssh"
-//    user     = "root"
-//    password = self.get_password_data
-//    host     = self.public_dns
-//  }
-//  provisioner "remote-exec" {
-//    inline = [
-//      "ls",
-//      "mkdir $HOME/code"
-//    ]
-//  }
-//
-//  provisioner "file" {
-//    source      = "../workflow"
-//    destination = "$HOME/code"
-//
-//  }
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("${path.root}/terraform-keys2")
+    host        = self.public_ip
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "ls",
+      "mkdir $HOME/code",
+      "mkdir $HOME/code/dags"
+    ]
+  }
+
+  provisioner "file" {
+    source      = "../workflow/dags/"
+    destination = "$HOME/code/dags"
+  }
+
+  provisioner "file" {
+    source      = "../workflow/docker-compose.yaml"
+    destination = "$HOME/code"
+  }
+
+  provisioner "file" {
+    source      = "../workflow/Makefile"
+    destination = "$HOME/code"
+  }
   user_data = <<EOF
 #!/bin/sh
 sudo apt-get update
 sudo apt-get install -y docker-compose
+make dev-setup
+make dev-run
 EOF
-  
+
   tags={
     Name = "product"
     Environment = "production"
